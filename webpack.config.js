cf082b2f956986de95ff48ec878b6991b8176b0c
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2017-12-21 17:02:22 +0100

    add example

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
@@ -19,5 +20,10 @@ module.exports = {
         }
       }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
   }
 }
